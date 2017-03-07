3909c1e2086829bf0c77de416df8cdbe33bbba5e
Zhao,Chengdong <zhaochengdong@baidu.com>
2017-03-07 09:11:47 +0800

    中间那张图片定位好了，其它图片还很乱

--- src/components/GallerByReactApp.js
+++ src/components/GallerByReactApp.js
@@ -6,6 +6,7 @@ var React = require('react/addons');
 require('normalize.css');
 require('../styles/main.scss');
 
+//获取图片想着数据
 var imageDatas = require('../data/imageDatas.json');
 
 //一个自执行函数来把json信息转化为路径
@@ -28,21 +29,23 @@ function getRangeRandom(low, high) {
 
 var ImgFigure = React.createClass({
     render: function() {
+
         var styleObj = {};
+
         if (this.props.arrange.pos) {
             styleObj = this.props.arrange.pos;
         }
         return ( < figure className = "image-figure"
-            styles = { styleObj } >
+            style = { styleObj } >
             < img height = "280" width = "240" src = { this.props.data.imageURL } alt = { this.props.data.title } /> < figcaption >
-             < h2 className = "img-title" > title < /h2 >
+             <h2 className = "img-title"> title </h2 >
              < /figcaption > < /figure >
         );
     }
 });
 
 var GallerByReactApp = React.createClass({
-            constant: {
+            Constant: {
                 centerPos: {
                     left: 0,
                     right: 0
@@ -61,28 +64,28 @@ var GallerByReactApp = React.createClass({
             //重新所有布局图片
             rearrange: function(centerIndex) {
                 var imgsArrangeArr = this.state.imgsArrangeArr,
-                    constant = this.constant,
-                    centerPos = constant.centerPos,
-                    hPosRange = constant.hPosRange,
-                    vPosRange = constant.vPosRange,
+                    Constant = this.Constant,
+                    centerPos = Constant.centerPos,
+                    hPosRange = Constant.hPosRange,
+                    vPosRange = Constant.vPosRange,
                     hPosRangeLeftSecX = hPosRange.leftSecX,
                     hPosRangeRightSecx = hPosRange.rightSecX,
                     hPosRangeY = hPosRange.y,
                     vPosRangeTopY = vPosRange.topY,
                     vPosRangeX = vPosRange.x,
                     imgsArrangeTopArr = [],
-                    topImgNum = Math.ceil((Math.random() * 2)), //取一个或者不取
+                    topImgNum = Math.ceil(Math.random() * 2), //取一个或者不取
                     topImgSpliceIndex = 0,
                     imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
-                    console.log(centerPos);
-                    //首先居中的centerIndex图片
-                    imgsArrangeCenterArr[0].pos = centerPos,
 
-                    //取出要布局上侧的图片的状态信息
+                    // 首先居中的centerIndex图片
+                    imgsArrangeCenterArr[0].pos = centerPos;
+
+                    // 取出要布局上侧的图片的状态信息
                     topImgSpliceIndex = Math.ceil(Math.random() * (imgsArrangeArr.length - topImgNum));
                     imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex, topImgNum);
 
-                    //布局位于上侧的图片
+                    // 布局位于上侧的图片
                     imgsArrangeTopArr.forEach(function(value, index) {
                     imgsArrangeTopArr[index].pos = {
                         top: getRangeRandom(vPosRangeTopY[0], vPosRangeTopY[1]),
@@ -93,6 +96,8 @@ var GallerByReactApp = React.createClass({
                 //布局两侧的图片信息
                 for (var i = 0, j = imgsArrangeArr.length, k = j / 2; i < j; i++) {
                     var hPosRangeLORX = null;
+
+                    //前半部分布左边，后半部分布局右边你
                     if (i < k) {
                         hPosRangeLORX = hPosRangeLeftSecX;
                     } else {
@@ -119,12 +124,12 @@ var GallerByReactApp = React.createClass({
             getInitialState: function() {
                 return {
                     imgsArrangeArr: [
-                        // {
-                        //     pos: {
-                        //         left: '0',
-                        //         top: '0'
-                        //     }
-                        // }
+                         {
+                             pos: {
+                                 left: '0',
+                                 top: '0'
+                             }
+                         }
                     ]
                 };
             },
@@ -136,12 +141,12 @@ var GallerByReactApp = React.createClass({
 
                 //首先拿到舞台的大小
                 var stageDOM = React.findDOMNode(this.refs.stage);
+                console.log(stageDOM.scrollHeight);
+                console.log(stageDOM.scrollWidth);
                 var stageW = stageDOM.scrollWidth,
                     stageH = stageDOM.scrollHeight,
                     halfStageW = Math.ceil(stageW / 2),
                     halfStageH = Math.ceil(stageH / 2);
-                    console.log(stageDOM.offsetWidth);//0
-                    console.log(stageH);//3025
 
                 //拿到imgFigure大小
                 var imgFigureDOM = React.findDOMNode(this.refs.imgFigure0),
@@ -149,26 +154,26 @@ var GallerByReactApp = React.createClass({
                     imgH = imgFigureDOM.scrollHeight,
                     halfImgW = Math.ceil(imgW / 2),
                     halfImgH = Math.ceil(imgH / 2);
-                    
+
                 //中间图片的位置
-                this.constant.centerPos = {
+                this.Constant.centerPos = {
                     left: halfStageW - halfImgW,
                     top: halfStageH - halfImgH
                 };
 
                 //计算左右区域图片位置的取值范围
-                this.constant.hPosRange.leftSecX[0] = -halfImgW;
-                this.constant.hPosRange.leftSecX[1] = halfImgW - halfImgW * 3;
-                this.constant.hPosRange.rightSecX[0] = halfStageW + halfImgW;
-                this.constant.hPosRange.rightSecX[1] = stageW - halfImgW;
-                this.constant.hPosRange.y[0] = -halfImgH;
-                this.constant.hPosRange.y[1] = stageH - halfImgH;
+                this.Constant.hPosRange.leftSecX[0] = -halfImgW;
+                this.Constant.hPosRange.leftSecX[1] = halfImgW - halfImgW * 3;
+                this.Constant.hPosRange.rightSecX[0] = halfStageW + halfImgW;
+                this.Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
+                this.Constant.hPosRange.y[0] = -halfImgH;
+                this.Constant.hPosRange.y[1] = stageH - halfImgH;
 
                 //计算上侧区域图片排布的取值范围
-                this.constant.vPosRange.topY[0] = -halfImgH;
-                this.constant.vPosRange.topY[1] = halfStageH - halfImgH * 3;
-                this.constant.vPosRange.x[0] = halfStageW - imgW;
-                this.constant.vPosRange.x[1] = halfImgW;
+                this.Constant.vPosRange.topY[0] = -halfImgH;
+                this.Constant.vPosRange.topY[1] = halfStageH - halfImgH * 3;
+                this.Constant.vPosRange.x[0] = halfStageW - imgW;
+                this.Constant.vPosRange.x[1] = halfImgW;
 
                 this.rearrange(0);
             },
@@ -190,16 +195,17 @@ var GallerByReactApp = React.createClass({
                             arrange = { this.state.imgsArrangeArr[index] }
                             />);
                         }.bind(this));
-                    return ( < section className = "stage" ref = "stage" >
-                                 < section className = "img-sec">{ ImgFigures }</section>
-                                 < nav className = "controller-nav" >{controlllerUnits }</nav>
-                             < /section >
-                    );
+                  return (
+                    <section className = "stage" ref = "stage" >
+                        < section className = "img-sec">{ ImgFigures }</section>
+                        < nav className = "controller-nav" >{controlllerUnits }</nav>
+                    </section >
+                  );
                 }
             });
 
-        React.render( < GallerByReactApp / >,
-            document.getElementById('content')
-        ); // jshint ignore:line
+React.render( < GallerByReactApp / >,
+    document.getElementById('content')
+); // jshint ignore:line
 
-        module.exports = GallerByReactApp;
+module.exports = GallerByReactApp;
